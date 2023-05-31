ea9982e80828c0c743c789949e6cbd55812a3f41
Bingobalazs <baluvilaga@gmail.com>
2023-05-31 22:34:32 +0200

    v1
    Természetesen nem működik. A projekt elkezdése után erre jutottam (egy délutánt vett időbe :)))))))))) )
    Random szuicid hajlam kódírás közben: #1
    Ennyiszer idegesített fel Csabika, úgy hogy két szónál többet nem is beszéltem vele: #3
    Kiakadás Róbertnek egy 6,5 perces hangüzenetben: #1
    De legalább működik. Vagyis nem...

--- script.js
+++ script.js
@@ -12,138 +12,142 @@ function kikerdezett() {
 
 function Kosaradd(cucc) {
     console.log(cucc)
+//ADATOK
+    let toldi = {
+        nev: "Toldi II",
+        hivo: cucc,
+        ar: 1263542999,
+        kep: "img/Toldi2.png",
+        kosarban: 0,
 
-
-    if (cucc == "toldi"){
-        let obj = {
-            nev: "Toldi II",
-            hivo: cucc,
-            ar: 1263542999,
-            kep: "img/Toldi2.png",
-            kosarban: 0,
-
-            torol: function () {
+        torol: function () {
             kosar.shift(this)
-                kosarprice-=ar
-                KosarDispl()
-            }
+            kosarprice-=ar
+            KosarDispl()
         }
+    }
+    let neuf = {
+        nev: "Neubaufahrzeug",
+        hivo: cucc,
+        ar: 3452283999,
+        kep: "img/neuf.png",
+        kosarban: 0,
+        torol: function () {
+            kosar.shift(this)
+            kosarprice-=this.ar
+            KosarDispl()
+        }
+    }
+    let alekosz = {
+        nev: "Mercedes-Benz S 320 CDI, 2006",
+        hivo: cucc,
+        ar: 19726999,
+        kep: "img/mercedesz.jpg",
+        kosarban: 0,
+        torol: function () {
+            kosar.shift(this)
+            kosarprice-=this.ar
+            KosarDispl()
+        }
+    }
+    let drip = {
+        nev: "Teljes outfit",
+        hivo: cucc,
+        ar: 99999,
+        kep: "img/drip.jpg",
+        kosarban: 0,
+        torol: function () {
+            kosar.shift(this)
+            kosarprice-=this.ar
+            KosarDispl()
+        }
+    }
+    let csabi = {
+        nev: "Csovács Kaba cossplay",
+        hivo: cucc,
+        ar: 299,
+        kep: "img/csabi.jpg",
+        kosarban: 0,
+
+        torol: function () {
+            kosar.shift(this)
+            kosarprice-=this.ar
+            KosarDispl()
+        }
+    }
+    let kalap = {
+        nev: "Nemzeti szalmakalap",
+        hivo: cucc,
+        ar: 2435999,
+        kep: "img/kalap.png",
+        kosarban: 0,
+
+        torol: function () {
+            kosar.shift(this)
+            kosarprice-=this.ar
+            KosarDispl()
+        }
+    }
+    let erettsegi = {
+        nev: "Érettségi Bizonyítvány (nem lopott)",
+        hivo: cucc,
+        ar: 4999,
+        kep: "img/erettsegi.jpg",
+        kosarban: 0,
+
+        torol: function () {
+            kosar.shift(this)
+            kosarprice-=this.ar
+            KosarDispl()
+        }
+    }
+    let tanarno = {
+        nev: "15 éves qgya",
+        hivo: cucc,
+        ar: 0,
+        kep: "img/kutya.jpg",
+        kosarban: 0,
+
+
+        torol: function  () {
+            kosar.shift(this)
+            kosarprice-=this.ar
+            KosarDispl()
+        }
+
+    }
+    if (cucc == "toldi"){
+
         kosar.push(obj)
-
-
-
     }else
     if (cucc == "neuf"){
-        let neuf = {
-            nev: "Neubaufahrzeug",
-            hivo: cucc,
-            ar: 3452283999,
-            kep: "img/neuf.png",
-            kosarban: 0,
-            torol: function () {
-                kosar.shift(this)
-                kosarprice-=this.ar
-                KosarDispl()
-            }
-        }
+        l
         kosar.push(obj)
 
     }else
     if (cucc == "alekosz"){
-        let alekosz = {
-            nev: "Mercedes-Benz S 320 CDI, 2006",
-            hivo: cucc,
-            ar: 19726999,
-            kep: "img/mercedesz.jpg",
-            kosarban: 0,
-            torol: function () {
-                kosar.shift(this)
-                kosarprice-=this.ar
-                KosarDispl()
-            }
-        }
+
         kosar.push(obj)
     }else
     if (cucc == "drip"){
-        let drip = {
-            nev: "Teljes outfit",
-            hivo: cucc,
-            ar: 99999,
-            kep: "img/drip.jpg",
-            kosarban: 0,
-            torol: function () {
-                kosar.shift(this)
-                kosarprice-=this.ar
-                KosarDispl()
-            }
-        }
+
         kosar.push(obj)
     }else
     if (cucc == "csabi"){
-        let csabi = {
-            nev: "Csovács Kaba cossplay",
-            hivo: cucc,
-            ar: 299,
-            kep: "img/csabi.jpg",
-            kosarban: 0,
 
-            torol: function () {
-                kosar.shift(this)
-                kosarprice-=this.ar
-                KosarDispl()
-            }
-        }
         kosar.push(obj)
     }else
     if (cucc == "kalap"){
-        let kalap = {
-            nev: "Nemzeti szalmakalap",
-            hivo: cucc,
-            ar: 2435999,
-            kep: "img/kalap.png",
-            kosarban: 0,
 
-            torol: function () {
-                kosar.shift(this)
-                kosarprice-=this.ar
-                KosarDispl()
-            }
-        }
         kosar.push(obj)
     }else
     if (cucc == "erettsegi"){
-        let erettsegi = {
-            nev: "Érettségi Bizonyítvány (nem lopott)",
-            hivo: cucc,
-            ar: 4999,
-            kep: "img/erettsegi.jpg",
-            kosarban: 0,
 
-            torol: function () {
-                kosar.shift(this)
-                kosarprice-=this.ar
-                KosarDispl()
-            }
-        }
         kosar.push(obj)
         kosarprice+=obj.ar
     }else
     if (cucc == "tanarno"){
-        let tanarno = {
-            nev: "15 éves qgya",
-            hivo: cucc,
-            ar: 0,
-            kep: "img/kutya.jpg",
-            kosarban: 0,
 
-
-            torol: function  () {
-                kosar.shift(this)
-                kosarprice-=this.ar
-                KosarDispl()
-            }
-
-        }
         kosar.push(obj)
 
 
@@ -153,7 +157,8 @@ function Kosaradd(cucc) {
     document.getElementById("cartcount").innerHTML=kosar.length
     kosarprice = (neuf.ar*neuf.kosarban)+(toldi.ar*toldi.kosarban)
         +(alekosz.ar*alekosz.kosarban)+(drip.ar*drip.kosarban)+
-        (neuf.ar*neuf.kosarban)
+        (csabi.ar*csabi.kosarban)+(kalap.ar*kalap.kosarban)+
+        (erettsegi.ar*erettsegi.kosarban)+
 
     document.getElementById("cartprice").innerHTML=kosarprice
 
